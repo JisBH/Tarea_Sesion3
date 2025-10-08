0c52f97faf1b673b0c849dbddbb8dd7897ac7baf
JisBH <soyjesusbh@gmail.com>
2025-10-08 11:07:33 +0200

    Paso 1. Hacer una prueba que falle

---
+++ src/paquete/Cuenta.java
@@ -0,0 +1,56 @@
+package paquete;
+
+public class Cuenta {
+	
+	private String numero;
+	private String titular;
+	private Double saldo;
+	
+	
+	public Cuenta(String numero, String titular, Double saldo) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+
+	public String getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+	public String getTitular() {
+		return titular;
+	}
+
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	public void ingresar(Double i) {
+				
+	}
+
+	public void retirar(Double i) {
+		
+	}
+
+
+}
